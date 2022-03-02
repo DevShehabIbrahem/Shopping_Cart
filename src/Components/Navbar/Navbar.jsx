75cf0d0112c88d3f6614f91fb1d6c010a9cc167d
DevShehabIbrahem <shehabibrahem222@gmail.com>
2022-03-01 18:19:21 -0800

    edit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,11 +8,10 @@ import {
   Badge,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ items }) => {
   const classes = useStyles();
-  const location = useLocation();
 
   return (
     <AppBar position="fixed" className={classes.appBar} color="inherit">
@@ -33,21 +32,19 @@ const Navbar = ({ items }) => {
           ShoppingCart
         </Typography>
         <div className={classes.grow} />
-        {location.pathname ===
-          "https://devshehabibrahem.github.io/Shopping_Cart" && (
-          <div className={classes.button}>
-            <IconButton
-              component={Link}
-              to="/cart"
-              aria-label="show cart item"
-              color="inherit"
-            >
-              <Badge badgeContent={items} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
-        )}
+
+        <div className={classes.button}>
+          <IconButton
+            component={Link}
+            to="/cart"
+            aria-label="show cart item"
+            color="inherit"
+          >
+            <Badge badgeContent={items} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
   );
